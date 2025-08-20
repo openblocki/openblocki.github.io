112a643e4c20b6e495083b4ddbc7b521f4c8a4a0
openblocki <58093113+openblocki@users.noreply.github.com>
2025-08-20 13:53:36 +0200

    blogPost logic begin

---
+++ main.js
@@ -0,0 +1,40 @@
+import { getAllPostMetadata } from "./js/services/postMetaService.js"; 
+import { PostMeta } from "./js/models/post_data.js";
+import { Octokit } from "https://esm.sh/@octokit/rest";
+const list = document.getElementById("post-list");
+
+console.log("main.js loaded");
+
+const owner = "openblocki";
+const repo = "openblocki.github.io";
+const branch_posts = "blogPosts";
+const directory_posts = "posts";
+const directory_posts_data = "data";
+const octokit = new Octokit();
+
+async function getPostMetadata() {
+    const postMeta = await getAllPostMetadata(owner, repo, branch_posts, directory_posts_data);
+    console.log("Post metadata fetched:", postMeta);
+    return postMeta.map(post => `${post.id}.html`);
+}
+
+const posts = await getPostMetadata().catch(error => {
+    console.error("Error fetching post metadata:", error)});
+
+async function renderFileList() {
+    const files = posts;
+    files.forEach(file => {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.href = `posts/${file.releaseDate.Year}/${file.id}`;
+        a.textContent = file.title;
+        li.appendChild(a);
+        list.appendChild(li);
+    });
+
+    console.log(files)
+}
+
+await renderFileList().catch(error => {
+    console.error("Error rendering file list:", error);
+});
